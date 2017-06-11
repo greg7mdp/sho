4c2fc2dea9152220f870f6b4e48e99ce4dccae5d
greg7mdp <greg7mdp@gmail.com>
2017-06-10 21:39:59 -0400

    start working on Short Hash Optimization.

---
+++ sho.h
@@ -0,0 +1,227 @@
+#ifndef sho__h__
+#define sho__h__
+
+#include <cstdint>
+#include <cassert>
+#include <algorithm>
+#include <utility>
+
+// ---------------------------------------------------------------------
+//            S M A L L    H A S H     O P T I M I Z A T I O N
+//            ------------------------------------------------
+// 
+// When you have a lot of very small hash maps with just zero to a couple 
+// entries => store a few element in a small array of possible, else 
+//            allocate a full hash_map.
+//
+// Assumptions:
+//     - we assume that the value of a pointer is always greater then N.
+// ---------------------------------------------------------------------
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+template <size_t N,
+          template <class, class, class, class, class> class RefMap,
+          class K,
+          class V, 
+          class Hash  = std::hash<K>, 
+          class Pred  = std::equal_to<K>, 
+          class Alloc = std::allocator<std::pair<const K, V> > >
+class sho
+{
+public:
+    typedef RefMap<K, V, Hash, Pred, Alloc>     Map;
+    typedef typename Map::key_type              key_type;
+    typedef typename Map::value_type            value_type;
+    typedef typename Map::hasher                hasher;
+    typedef typename Map::key_equal             key_equal;
+    typedef typename Map::allocator_type        allocator_type;
+    typedef std::pair<K, V>                     mutable_value_type;
+
+    typedef typename Map::size_type             size_type;
+    typedef typename Map::difference_type       difference_type;
+
+    typedef typename Map::iterator              map_iterator;
+
+    // ----------------
+    template <class Val>
+    class Iterator
+    {
+    public:
+        typedef Val value_type;
+        typedef typename sho::Map::iterator map_iter;
+
+        explicit Iterator(const value_type *p) : _p((value_type *)p) {}
+        explicit Iterator(const map_iter &o) : _p(0), _it(o) {}
+        Iterator(const Iterator &o) : _p(o._p), _it(o._it) {}
+        Iterator& operator++() { if (_p) ++_p; else ++_it; }
+        Iterator& operator--() { if (_p) --_p; else --_it; }
+        bool operator==(const Iterator &o) const { return _p ? _p == o._p : _it == o._it; }
+        bool operator!=(const Iterator &o) const { return !(*this == o); }
+        value_type& operator*() const  { return _p ? *_p : *_it; }
+        value_type* operator->() const { return &(operator*()); }
+
+    private:
+        friend class sho;
+        
+        value_type *_p;
+        map_iter    _it;
+    };
+
+    // ----------------
+    typedef Iterator<value_type>        iterator;
+    typedef Iterator<const value_type>  const_iterator;
+
+    sho(size_type = 0) : _cnt(0) {}
+
+    ~sho() 
+    { 
+        if (_hasMap()) 
+            _cleanMap();
+    }
+        
+    sho(const sho &o) : _cnt(0) 
+    {
+        if (o._hasMap())
+            _cnt = (uintptr_t)(new Map(o._getMap()));
+        else
+            _copyLocal(o);
+    }
+
+    sho& operator=(const sho &o) 
+    {
+        if (this == &o)
+            return *this;
+
+        if (_hasMap()) 
+            _cleanMap();
+        if (o._hasMap()) 
+            _cnt = (uintptr_t)(new Map(o._getMap()));
+        else
+            _copyLocal(o);
+        return *this;
+    }
+    
+    // Iterator functions
+    iterator begin() const             
+    {
+        return _hasMap() ? iterator(_getMap()->begin()) : iterator(&_items[0]); 
+    }
+    
+    iterator end() const             
+    {
+        return _hasMap() ? iterator(_getMap()->end()) : iterator(&_items[N]); 
+    }
+
+    const_iterator cbegin() const            
+    {
+        return _hasMap() ? const_iterator(_getMap()->begin()) : const_iterator(&_items[0]); 
+    }
+    
+    const_iterator cend() const             
+    {
+        return _hasMap() ? const_iterator(_getMap()->end()) : const_iterator(&_items[N]); 
+    }
+ 
+    iterator find(const key_type& key)
+    {
+        if (_hasMap())
+            return iterator(_getMap()->find(key));
+        else
+            for (size_t i=0; i<_cnt; ++i)
+                if (key_equal()(_items[i].first, key))
+                    return iterator(&_items[i]); 
+        return end();
+    }
+
+    const_iterator find(const key_type& key) const
+    {
+        if (_hasMap())
+            return const_iterator(_getMap()->find(key));
+        else
+            for (size_t i=0; i<_cnt; ++i)
+                if (key_equal()(_items[i].first, key))
+                    return const_iterator(&_items[i]); 
+        return cend();
+    }
+
+    std::pair<iterator, bool> insert(const value_type& val)
+    {
+        if (_hasMap())
+        {
+            std::pair<map_iterator, bool> res = _getMap()->insert(val);
+            return std::make_pair(iterator(res.first), res.second);
+        }
+
+        for (size_t i=0; i<_cnt; ++i)
+            if (key_equal()(_items[i].first, val.first))
+                return std::make_pair(iterator(&_items[i]), false);
+
+        // not present ... insert it
+        if (_cnt == N)
+        {
+            _switchToMap();
+            std::pair<map_iterator, bool> res = _getMap()->insert(val);
+            assert(res.second);
+            return std::make_pair(iterator(res.first), true);
+        }
+        *((K *)(&_items[_cnt].first)) = val.first;
+        _items[_cnt].second = val.second;
+        return std::make_pair(iterator(&_items[_cnt++]), true);
+    }
+
+    size_type size() const { return _hasMap() ? _getMap()->size() : _cnt; }
+
+    void clear() 
+    {
+        if (_hasMap())
+            _cleanMap();
+        else
+        {
+            allocator_type alloc;
+            for (size_t i=0; i<_cnt; ++i)
+                alloc.destruct(_items[i]);
+            _cnt = 0;
+        }
+    }
+        
+private:
+    bool   _hasMap() const { return _cnt > N; }
+    
+    Map *_getMap() const { assert(_hasMap()); return (Map *)_cnt; }
+
+    void   _cleanMap() { delete _getMap(); _cnt = 0; }
+
+    void   _copyLocal(const sho &o) 
+    {
+        assert(!_hasMap());
+        _cnt = o._cnt;
+        for (size_t i=0; i<_cnt; ++i)
+            _items[i] = o._items[i];
+    }
+
+    void   _createMap()
+    { 
+        assert(!_hasMap()); 
+        _cnt = (uintptr_t)(new Map);
+    }
+
+    void   _switchToMap()
+    {
+        assert(!_hasMap());
+
+        uintptr_t cnt = _cnt;
+        _cnt = 0;
+        _createMap();
+
+        Map *map = _getMap();
+        for (size_t i=0; i<cnt; ++i)
+            map->insert(_items[i]);
+    }
+ 
+    uintptr_t  _cnt;
+    value_type _items[N];
+};
+
+
+#endif // sho__h__
---
+++ test/test_sho.cc
@@ -0,0 +1,223 @@
+// compile on windows with: CL -O2 -I.. -EHsc test_sho.cc -o test_sho
+// ----------------------------------------------------------------------
+#include <memory>
+#include <cassert>
+#include <cstdio>
+#include <cstdint>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm> 
+#include <vector>
+#include <chrono>
+
+#include <unordered_map>
+#include <sho.h>
+
+// -----------------------------------------------------------
+//                     Timer
+// -----------------------------------------------------------
+namespace dltest
+{
+    template<typename time_unit = std::milli>
+    class Timer 
+    {
+    public:
+        Timer()                 { reset(); }
+        void reset()            { _start = _snap = clock::now();  }
+        void snap()             { _snap = clock::now();  }
+
+        float get_total() const { return get_diff<float>(_start, clock::now()); }
+        float get_delta() const { return get_diff<float>(_snap, clock::now());  }
+        
+    private:
+        using clock = std::chrono::high_resolution_clock;
+        using point = std::chrono::time_point<clock>;
+
+        template<typename T>
+        static T get_diff(const point& start, const point& end) 
+        {
+            using duration_t = std::chrono::duration<T, time_unit>;
+
+            return std::chrono::duration_cast<duration_t>(end - start).count();
+        }
+
+        point _start;
+        point _snap;
+    };
+}
+
+// -----------------------------------------------------------
+//                     Memory usage
+// -----------------------------------------------------------
+#if defined(_WIN32) || defined( __CYGWIN__)
+    #define SHO_TEST_WIN
+#endif
+
+#ifdef SHO_TEST_WIN
+    #include <windows.h>
+    #include <Psapi.h>
+    #undef min
+    #undef max
+#else
+    #include <sys/types.h>
+    #include <sys/sysinfo.h>
+#endif
+
+namespace dltest
+{
+    uint64_t GetSystemMemory()
+    {
+#ifdef SHO_TEST_WIN
+        MEMORYSTATUSEX memInfo;
+        memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+        GlobalMemoryStatusEx(&memInfo);
+        return static_cast<uint64_t>(memInfo.ullTotalPageFile);
+#else
+        struct sysinfo memInfo;
+        sysinfo (&memInfo);
+        auto totalVirtualMem = memInfo.totalram;
+
+        totalVirtualMem += memInfo.totalswap;
+        totalVirtualMem *= memInfo.mem_unit;
+        return static_cast<uint64_t>(totalVirtualMem);
+#endif
+    }
+
+    uint64_t GetTotalMemoryUsed()
+    {
+#ifdef SHO_TEST_WIN
+        MEMORYSTATUSEX memInfo;
+        memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+        GlobalMemoryStatusEx(&memInfo);
+        return static_cast<uint64_t>(memInfo.ullTotalPageFile - memInfo.ullAvailPageFile);
+#else
+        struct sysinfo memInfo;
+        sysinfo(&memInfo);
+        auto virtualMemUsed = memInfo.totalram - memInfo.freeram;
+
+        virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
+        virtualMemUsed *= memInfo.mem_unit;
+
+        return static_cast<uint64_t>(virtualMemUsed);
+#endif
+    }
+
+    uint64_t GetProcessMemoryUsed()
+    {
+#ifdef SHO_TEST_WIN
+        PROCESS_MEMORY_COUNTERS_EX pmc;
+        GetProcessMemoryInfo(GetCurrentProcess(), reinterpret_cast<PPROCESS_MEMORY_COUNTERS>(&pmc), sizeof(pmc));
+        return static_cast<uint64_t>(pmc.PrivateUsage);
+#else
+        auto parseLine = 
+            [](char* line)->int
+            {
+                auto i = strlen(line);
+				
+                while(*line < '0' || *line > '9') 
+                {
+                    line++;
+                }
+
+                line[i-3] = '\0';
+                i = atoi(line);
+                return i;
+            };
+
+        auto file = fopen("/proc/self/status", "r");
+        auto result = -1;
+        char line[128];
+
+        while(fgets(line, 128, file) != nullptr)
+        {
+            if(strncmp(line, "VmSize:", 7) == 0)
+            {
+                result = parseLine(line);
+                break;
+            }
+        }
+
+        fclose(file);
+        return static_cast<uint64_t>(result) * 1024;
+#endif
+    }
+
+    uint64_t GetPhysicalMemory()
+    {
+#ifdef SHO_TEST_WIN
+        MEMORYSTATUSEX memInfo;
+        memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+        GlobalMemoryStatusEx(&memInfo);
+        return static_cast<uint64_t>(memInfo.ullTotalPhys);
+#else
+        struct sysinfo memInfo;
+        sysinfo(&memInfo);
+
+        auto totalPhysMem = memInfo.totalram;
+
+        totalPhysMem *= memInfo.mem_unit;
+        return static_cast<uint64_t>(totalPhysMem);
+#endif
+    }
+}
+
+// -----------------------------------------------------------
+//                        test code
+// -----------------------------------------------------------
+using namespace std;
+
+static float _to_mb(uint64_t m) { return (float)((double)m / (1024 * 1024)); }
+
+// -----------------------------------------------------------
+// -----------------------------------------------------------
+template <class Hash, class K, class V>
+void run_test(const char *alloc_name, size_t num_iter)
+{
+    printf("---------------- testing %s\n", alloc_name);
+
+    printf("\nmem usage before the test: %4.1f\n", _to_mb(dltest::GetProcessMemoryUsed()));
+    srand(43); // always same sequence of random numbers
+
+    Hash *hashes = new Hash[num_iter];
+    
+    dltest::Timer<std::milli> timer;
+
+    for (size_t i=0; i<num_iter; ++i)
+    {
+        size_t num_insert = rand() % 4;
+        if (i % 5000 == 0)
+            num_insert = 10000;
+
+        K inserted = 0;
+        for (uint32_t j=0; j<num_insert; ++j)
+        {
+            inserted = rand();
+            hashes[i].insert(std::make_pair((K)inserted, (V)0));
+        }
+
+        if (inserted && hashes[i].find(inserted) == hashes[i].end())
+            abort();
+        
+    }
+    printf("mem usage after hashes created and filled: %4.1f\n",
+           _to_mb(dltest::GetProcessMemoryUsed()));
+    printf("       in %3.2f seconds\n", timer.get_total() / 1000);
+
+    delete [] hashes;
+    printf("mem usage after hashes deleted: %4.1f\n",
+           _to_mb(dltest::GetProcessMemoryUsed()));
+
+    printf("\n");
+}
+
+
+// -----------------------------------------------------------
+// -----------------------------------------------------------
+int main()
+{
+    typedef std::unordered_map<uint32_t, void *> StdMap;
+    typedef sho<2, std::unordered_map, uint32_t, void *>        ShoMap;
+
+    run_test<StdMap, uint32_t, void *>("unordered_map", 5000000);
+    run_test<ShoMap, uint32_t, void *>("unordered_map with sho", 5000000);
+}
